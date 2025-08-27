c1251c336f1d14e28217cd3b22f96f9289d08e3f
Olena <olena@example.com>
2025-08-27 17:38:58 +0300

    Update logo to Verco and change welcome text from Natasha to Andries across all views

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -43,7 +43,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={customerLogoSrc || "/logos/Company logos/Energise.svg"}
+              src={customerLogoSrc || "/logos/Company logos/Verco.svg"}
               alt={`${customerName} logo`}
               className="h-[48px] w-auto object-contain"
               onError={(e) => {
@@ -105,7 +105,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
             </div>
           )}
           
-          <Avatar name="Natasha Hall" size={36} />
+          <Avatar name="Andries Hall" size={36} />
         </div>
       </div>
     </div>
